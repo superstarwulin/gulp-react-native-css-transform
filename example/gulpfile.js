9721a7a15c2841518d31edcc5fb439170be582b3
superstarwulin <superstarwulin@gmail.com>
2016-12-19 10:39:36 +0800

    update

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,13 +2,16 @@ var gulp = require('gulp');
 
 var rncss = require('../index');
 
-gulp.task('css2js', function () {
-    gulp.src(['*.css'])
-        .pipe(rncss())
-        .pipe(gulp.dest('./rnStyle'));
+var path = '/Users/zhuzhuxia/WebstormProjects/hpk/app/ytmallApp/ytmallApp/';
 
+var sourceArray = [path+'src/*.css',path+'src/**/*.css',path+'src/**/**/*.css'];
+
+gulp.task('css2js', function () {
+    gulp.src(sourceArray)
+        .pipe(rncss())
+        .pipe(gulp.dest(path + 'src'));
 });
 
 gulp.task('default', ['css2js'], function () {
-    gulp.watch(['*.css'], ['css2js']);
+    gulp.watch(sourceArray, ['css2js']);
 });
